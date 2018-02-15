35077ad4dac711400bd807309c83ec2047232bdd
Eyþór <eye23@hi.is>
2018-02-15 19:14:59 +0000

    tók til í kóðanum

--- Spectrum.c
+++ Spectrum.c
@@ -6,19 +6,8 @@
 #include "CortexM.h"
 #include "FFT.h"
 
-uint32_t sqrt32(uint32_t s);
 
-//---------------- Global variables shared between tasks ----------------
-uint16_t SoundData;         // raw data sampled from the microphone
-
-int ReDrawAxes;
-enum plotstate{
-  FFTplot,
-  Microphone,
-	Bins
-};
-enum plotstate PlotState = FFTplot;
-
+// Global Variables
 //color constants
 #define BGCOLOR     LCD_BLACK
 #define AXISCOLOR   LCD_ORANGE
@@ -28,32 +17,28 @@ enum plotstate PlotState = FFTplot;
 #define LIGHTCOLOR  LCD_LIGHTGREEN
 #define TOPTXTCOLOR LCD_WHITE
 #define TOPNUMCOLOR LCD_ORANGE
-//------------ end of Global variables shared between tasks -------------
 
-//---------------- Task0 samples sound from microphone ----------------
-//#define SOUNDRMSLENGTH 10   // number of samples to collect before calculating RMS (may overflow if greater than 4104)
-#define SOUNDRMSLENGTH 1000 // number of samples to collect before calculating RMS (may overflow if greater than 4104)
+
 #define fftLength 512
 #define logfft 9
-int kfft;
-int16_t SoundArray[SOUNDRMSLENGTH];
-double real[fftLength];
+uint16_t SoundData;					// raw data sampled from the microphone
+double real[fftLength]; 			// geymir sample fyrir fft og svo niðurstöðu
 double imag[fftLength];
-double V[10] = {3,4,5,6,7,7,6,5,4,3};
+double V[10];						// FFT Bins
+int ReDrawAxes = 1;					// endurteiknar Axes ef != 0
+uint32_t sampleTime, FFTtime;		// Stopwatch
+int kfft; // skoða þetta
 
-// *********Task0_Init*********
-// initializes microphone
-// Task0 measures sound intensity
-// Inputs:  none
-// Outputs: none
-void Task0_Init(void){
-  BSP_Microphone_Init();
-}
+// Task 4 teiknar eftir hvað plotstate er
+enum plotstate{
+	FFTplot,
+	Microphone,
+	Bins
+};
+enum plotstate PlotState = FFTplot;
 
-// *********Task0*********
-// calculate fft bins
-// Inputs:  none
-// Outputs: none
+//---------------- Task1 Bins ----------------
+// Calculate FFT bins
 void Task0(void){
 	V[0] = (real[1]-420)/10;
 	V[1] = (real[2]-420)/10;
@@ -76,23 +61,17 @@ void Task0(void){
 	}
 	V[9] = 4*(V[9]/23-420)/10;
 }
-/* ****************************************** */
-/*          End of Task0 Section              */
-/* ****************************************** */
 
-//---------------- Task1 Sample FFT ----------------
-uint32_t sampleTime, FFTtime;
 
-// *********Task1*********
-// Calculate fft in real[]
-// Inputs:  none
-// Outputs: none
+//---------------- Task1 FFT  ----------------
+// Sample fftlength microphone samples
+// Breytir real[] í FFT útreikninga,
+// skrifar sample tíma í sampleTime
+// og FFT tíma í FFTtime
 void Task1(void){
-  TExaS_Task1();     // record system time in array, toggle virtual logic analyzer
-  Profile_Toggle1(); // viewed by the logic analyzer to know Task1 started
-	
 	uint32_t t0, t1;
 	
+	//microphone sample
 	t0 = BSP_Time_Get();
 	for(kfft = 0; kfft < fftLength; kfft++){
 		BSP_Microphone_Input(&SoundData);
@@ -100,7 +79,8 @@ void Task1(void){
 	}
 	t1 = BSP_Time_Get();
 	sampleTime = t1-t0;
-
+	
+	// FFT útreikningar
 	t0 = BSP_Time_Get();
 	FFT(1, logfft, real, imag);
 	for ( int i = 0; i < fftLength; i++){
@@ -113,118 +93,60 @@ void Task1(void){
 	FFTtime = t1-t0;
 }
 
-
-/* ****************************************** */
-/*          End of Task1 Section              */
-/* ****************************************** */
-
-//------------Task3 handles switch input -------
-// *********Task3_Init*********
-// initializes switches
-// Task3 checks the switches, updates the mode
-// Inputs:  none
-// Outputs: none
-void Task3_Init(void){
-  BSP_Button1_Init();
-  BSP_Button2_Init();
-}
-// *********Task3*********
-// non-real-time task
-// checks the switches, updates the mode
-// Inputs:  none
-// Outputs: none
+//---------------- Task3 Takkar ---------------
+// Fylgist með tökkum og uppfærir mode
 void Task3(void){
-  static uint8_t prev1 = 0, prev2 = 0;
-  uint8_t current;
-  TExaS_Task3();     // record system time in array, toggle virtual logic analyzer
-  Profile_Toggle3(); // viewed by the logic analyzer to know Task3 started
-
-
-  current = BSP_Button1_Input();
-  if((current == 0) && (prev1 != 0)){
-    // Button1 was pressed since last loop
-    if(PlotState == FFTplot){
+	static uint8_t prev1 = 0, prev2 = 0;
+	uint8_t current;
+	current = BSP_Button1_Input();
+	if((current == 0) && (prev1 != 0)){
+		// Button1 was pressed since last loop
+		if(PlotState == FFTplot){
 			BSP_LCD_FillScreen(BSP_LCD_Color565(0, 0, 0));
-      PlotState = Bins;
-    }else if(PlotState == Bins){
+			PlotState = Bins;
+		}else if(PlotState == Bins){
 			BSP_LCD_FillScreen(BSP_LCD_Color565(0, 0, 0));
 			PlotState = Microphone;
 			ReDrawAxes = 1; 
 		}else if(PlotState == Microphone){
-		  PlotState = FFTplot;
-			ReDrawAxes = 1; 
+			  PlotState = FFTplot;
+				ReDrawAxes = 1; 
 		}
-  }
-  prev1 = current;
-  current = BSP_Button2_Input();
-  if((current == 0) && (prev2 != 0)){
-    // Button2 was pressed since last loop
-    if(PlotState == FFTplot){
+	}
+	prev1 = current;
+	// button 2 gerir það sama og 1
+	// geymi þetta fyrir future feature
+	current = BSP_Button2_Input();
+	if((current == 0) && (prev2 != 0)){
+		// Button2 was pressed since last loop
+		if(PlotState == FFTplot){
 			BSP_LCD_FillScreen(BSP_LCD_Color565(0, 0, 0));
-      PlotState = Bins;
-    }else if(PlotState == Bins){
+			PlotState = Bins;
+		}else if(PlotState == Bins){
 			BSP_LCD_FillScreen(BSP_LCD_Color565(0, 0, 0));
 			PlotState = Microphone;
 			ReDrawAxes = 1; 
 		}else if(PlotState == Microphone){
-		  PlotState = FFTplot;
-			ReDrawAxes = 1; 
+			  PlotState = FFTplot;
+				ReDrawAxes = 1; 
 		}
-  }
-  prev2 = current;
-}
-/* ****************************************** */
-/*          End of Task3 Section              */
-/* ****************************************** */
-
-//---------------- Task4 plots data on LCD ----------------
-#define SOUND_MAX 900
-#define SOUND_MIN 300
-void drawaxes(void){
-  if(PlotState == FFTplot){
-		BSP_LCD_Drawaxes(AXISCOLOR, BGCOLOR, "freq", "Mag", MAGCOLOR, "", 0, SoundData+100, SoundData-100);
-	} else if(PlotState == Microphone){
-    BSP_LCD_Drawaxes(AXISCOLOR, BGCOLOR, "Time", "Sound", SOUNDCOLOR, "", 0, SoundData+100, SoundData-100);
-  }
-}
-// return the number of digits
-int numlength(uint32_t n){
-  if(n < 10) return 1;
-  if(n < 100) return 2;
-  if(n < 1000) return 3;
-  if(n < 10000) return 4;
-  if(n < 100000) return 5;
-  if(n < 1000000) return 6;
-  if(n < 10000000) return 7;
-  if(n < 100000000) return 8;
-  if(n < 1000000000) return 9;
-  return 10;
+	}
+	prev2 = current;
 }
 
-// *********Task4_Init*********
-// initializes LCD
-// Task4 updates the plot and Task5 updates the text at the top of the plot
-// Inputs:  none
-// Outputs: none
-void Task4_Init(void){
-  BSP_LCD_Init();
-  BSP_LCD_FillScreen(BSP_LCD_Color565(0, 0, 0));
-  drawaxes();
-  ReDrawAxes = 1;
-}
-// *********Task4*********
-// updates the plot
-// Inputs:  none
-// Outputs: none
+
+//---------------- Task4 LCD ---------------
+// Plottar  upplýsingar á skjáinn m.v PlotState
 void Task4(void){
-	TExaS_Task4();     // record system time in array, toggle virtual logic analyzer
-  Profile_Toggle4(); // viewed by the logic analyzer to know Task4 started
-
-  if(ReDrawAxes){
-    ReDrawAxes = 0;
-    drawaxes();
-  }
-  if(PlotState == FFTplot){
+	if(ReDrawAxes){
+		ReDrawAxes = 0;
+		if(PlotState == FFTplot){
+			BSP_LCD_Drawaxes(AXISCOLOR, BGCOLOR, "freq", "Mag", MAGCOLOR, "", 0, SoundData+100, SoundData-100);
+		} else if(PlotState == Microphone){
+			BSP_LCD_Drawaxes(AXISCOLOR, BGCOLOR, "Time", "Sound", SOUNDCOLOR, "", 0, SoundData+100, SoundData-100);
+		}
+	}
+	if(PlotState == FFTplot){
 		for ( int i = 1; i < 51; i++){
 			BSP_LCD_PlotPoint(real[i], SOUNDCOLOR);
 			BSP_LCD_PlotIncrement();
@@ -235,12 +157,11 @@ void Task4(void){
 		BSP_LCD_SetCursor(12, 0); BSP_LCD_OutUDec(sampleTime, LIGHTCOLOR);
 		BSP_LCD_DrawString(2, 1,  "FFT=",  TOPTXTCOLOR);
 		BSP_LCD_SetCursor(12, 1); BSP_LCD_OutUDec(FFTtime, LIGHTCOLOR);
-  }else if(PlotState == Microphone){
+	} else if(PlotState == Microphone){
 		BSP_Microphone_Input(&SoundData);
-    BSP_LCD_PlotPoint(SoundData, SOUNDCOLOR);
+		BSP_LCD_PlotPoint(SoundData, SOUNDCOLOR);
 		BSP_LCD_PlotIncrement();
-  }
-	else if(PlotState == Bins){
+	} else if(PlotState == Bins){
 		for(int i=0; i<=9; i++){ 
 			for(int j=0; j<=9; j++){
 				if(j <= V[i]){
@@ -251,59 +172,34 @@ void Task4(void){
 			}
 		}
 	}
-
 }
-/* ****************************************** */
-/*          End of Task4 Section              */
-/* ****************************************** */
 
 
 int main(void){
-  DisableInterrupts();
-  BSP_Clock_InitFastest();
-  Profile_Init();               // initialize the 7 hardware profiling pins
-  // change 1000 to 4-digit number from edX 
-  TExaS_Init(GRADER, 1000 );         // initialize the Lab 1 grader
-//  TExaS_Init(LOGICANALYZER, 1000);  // initialize the Lab 1 logic analyzer
-  Task0_Init();    // microphone init
-	Task3_Init();    // buttons init
-  Task4_Init();    // LCD graphics init
+	DisableInterrupts();
+	BSP_Clock_InitFastest();	// BSP_Clock_GetFreq(); gefur klukkuhraða
+	Profile_Init();						// initialize the 7 hardware profiling pins
 
-  EnableInterrupts(); // interrupts needed for grader to run
-	//for(int i = 0; i<128; i++){
-	//	Sinewave[i] = 0.0;
-	//}
-	//Sinewave[64] = 500.0;
-	ReDrawAxes = 1;
+	// Virkar ekki án, ekki hugmynd hvað þetta gerir
+	TExaS_Init(GRADER, 1000 ); // initialize the Lab 1 GRADER/LOGICANALYZER
+	
+	BSP_Button1_Init();
+	BSP_Button2_Init();
+	BSP_Microphone_Init();
+	BSP_LCD_Init();
+	BSP_LCD_FillScreen(BSP_LCD_Color565(0, 0, 0));
+	EnableInterrupts(); // interrupts needed for grader to run
+
 	while(1){
-		
 		if(PlotState == FFTplot){
 			Task1(); // samp mic and calc fft
 		}
 		if(PlotState == Bins){
 			Task1(); // samp mic and calc fft
-			Task0();
+			Task0(); // Calculate Bins
 		}
 		Task3();  // check the buttons and change mode if pressed
 		Task4();  // update the plot
-		
-    Profile_Toggle6();
   }
 }
-
-// Newton's method
-// s is an integer
-// sqrt(s) is an integer
-uint32_t sqrt32(uint32_t s){
-uint32_t t;   // t*t will become s
-int n;             // loop counter
-  t = s/16+1;      // initial guess
-  for(n = 16; n; --n){ // will finish
-    t = ((t*t+s)/t)/2;
-  }
-  return t;
-}
-
-
-
 
