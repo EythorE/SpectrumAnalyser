19a44e392e8d1b3af0ef543317f7f04435fe6033
Eyþór <eye23@hi.is>
2018-02-15 16:39:33 +0000

    Breytti nafninu á keil projectinu úr Lab1 í Spectrum Analyser

---
+++ Objects/spectrum.d
@@ -0,0 +1,8 @@
+.\objects\spectrum.o: Spectrum.c
+.\objects\spectrum.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\spectrum.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\spectrum.o: ../inc/BSP.h
+.\objects\spectrum.o: ../inc/Profile.h
+.\objects\spectrum.o: Texas.h
+.\objects\spectrum.o: ../inc/CortexM.h
+.\objects\spectrum.o: FFT.h
